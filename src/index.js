859481ebe75285604787d5f2c89c23611544bfb5
Inje Yeo <inje@Injes-MBP.citidel>
2017-12-13 16:09:50 -0800

    Index: Export added components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export { default as Button } from './Button';
+import Button from './Button';
+import Header from './Header';
+import Link from './Link';
+import Text from './Text';
+
+export { Button, Header, Link, Text };
