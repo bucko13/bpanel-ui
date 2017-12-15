16020b178cbece0d4adb39badd2c43e3b72e31bf
Inje Yeo <inje@Injes-MBP.citidel>
2017-12-15 12:47:32 -0800

    Table: Add Table component

---
+++ src/components/Table/Table.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import {
+  AutoSizer,
+  Table as VirtualizedTable,
+  Column
+} from 'react-virtualized';
+
+import 'react-virtualized/styles.css';
+import './Table.scss';
+
+export default class Table extends PureComponent {
+  static get propTypes() {
+    return {
+      colProps: PropTypes.arrayOf(
+        PropTypes.shape({
+          label: PropTypes.string,
+          dataKey: PropTypes.string,
+          width: PropTypes.number
+        })
+      )
+    };
+  }
+
+  render() {
+    const { colProps, tableContainerStyles, ...tableProps } = this.props;
+    return (
+      <div style={tableContainerStyles}>
+        <AutoSizer disableHeight style={tableProps.style}>
+          {({ width }) => (
+            <VirtualizedTable {...tableProps} width={width}>
+              {colProps.map(colProp => (
+                <Column key={`table-${colProps.dataKey}`} {...colProp} />
+              ))}
+            </VirtualizedTable>
+          )}
+        </AutoSizer>
+      </div>
+    );
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from './Button';
 import Header from './Header';
 import Link from './Link';
 import Text from './Text';
+import Table from './Table';
 
-export { Button, Header, Link, Text };
+export { Button, Header, Link, Text, Table };
