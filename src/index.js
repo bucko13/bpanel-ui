cb4ad5b09648ec2bc820dc0ce196abe8775a8631
Inje Yeo <inje@Injes-MBP.citidel>
2018-01-29 14:21:49 -0800

    Export: Restructure exports

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@ import * as constants from './constants';
 import * as utils from './utils';
 
 const { Button, Header, Link, SidebarNavItem, Table, Text } = components;
+const { HEADER_CONSTANTS, TEXT_CONSTANTS } = constants;
+const { connectTheme, makeGutter, makeRem } = utils;
 
-export { Button, Header, Link, SidebarNavItem, Table, Text, constants, utils };
+export {
+  /* Components */
+  Button,
+  Header,
+  Link,
+  SidebarNavItem,
+  Table,
+  Text,
+  /* Constants */
+  HEADER_CONSTANTS,
+  TEXT_CONSTANTS,
+  /* Utils */
+  connectTheme,
+  makeGutter,
+  makeRem
+};
