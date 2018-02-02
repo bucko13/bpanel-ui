d999be1b958cf05cb0f14b50ef6f511553d02f51
Inje Yeo <inje@Injes-MBP.citidel>
2018-02-02 15:02:34 -0800

    TabMenu: Add TabMenu

---
+++ src/components/TabMenu/TabMenu.js
@@ -0,0 +1,103 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import Text from '../Text';
+import { connectTheme } from '../../utils';
+
+const defaultTabs = [
+  { header: 'Tab 1', body: <div>Welcome to bPanel!</div> },
+  { header: 'Tab 2', body: <div>Tab 2 content</div> },
+  { header: 'Tab 3', body: <div>Tab 3 content</div> }
+];
+
+class TabMenu extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      selectedIndex: 0
+    };
+  }
+
+  static get propTypes() {
+    return {
+      tabs: PropTypes.arrayOf(PropTypes.object)
+    };
+  }
+
+  static get defaultProps() {
+    return {
+      style: { tabMenu: {} },
+      tabs: defaultTabs,
+      theme: { tabMenu: {} }
+    };
+  }
+
+  renderHeader(header, index) {
+    return (
+      <div
+        onClick={e => {
+          this.setState({ selectedIndex: index });
+        }}
+      >
+        <Text>{header}</Text>
+      </div>
+    );
+  }
+
+  render() {
+    const {
+      tabs,
+      theme: {
+        tabMenu: {
+          headerContainer,
+          headerText,
+          headerTextActive,
+          headerTextInactive,
+          bodyContainer,
+          bodyActive,
+          bodyInactive
+        }
+      },
+      style: {
+        tabMenu: {
+          customHeaderContainer = {},
+          customHeaderText = {},
+          customHeaderTextActive = {},
+          customHeaderTextInactive = {},
+          customBodyContainer = {},
+          customBodyActive = {},
+          customBodyInactive = {}
+        }
+      },
+      ...otherProps
+    } = this.props;
+    const { selectedIndex } = this.state;
+
+    return (
+      <div>
+        <div style={{ ...headerContainer, ...customHeaderContainer }}>
+          {tabs.map(({ header }, index) => {
+            const selected = selectedIndex === index;
+            const stateStyles = selected
+              ? { ...headerTextActive, ...customHeaderTextActive }
+              : { ...headerTextInactive, ...customHeaderTextInactive };
+            return (
+              <div style={{ ...headerText, ...stateStyles }}>
+                {this.renderHeader(header, index)}
+              </div>
+            );
+          })}
+        </div>
+        {tabs.map(({ body }, index) => {
+          const selected = selectedIndex === index;
+          const stateStyles = selected
+            ? { ...bodyActive, ...customBodyActive }
+            : { ...bodyInactive, ...customBodyInactive };
+          return <div style={{ ...bodyContainer, ...stateStyles }}>{body}</div>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default connectTheme(TabMenu);
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Link from './Link';
 import SidebarNavItem from './SidebarNavItem';
 import Table from './Table';
+import TabMenu from './TabMenu';
 import Text from './Text';
 
-export { Button, Header, Link, SidebarNavItem, Table, Text };
+export { Button, Header, Link, SidebarNavItem, Table, TabMenu, Text };
