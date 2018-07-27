3115b197dd040243feeed5f268c58e4393fca3db
bucko13 <bucko.perley@gmail.com>
2018-07-27 16:00:45 -0700

    make sidebar item unopinionated about Link path and use local Link component to support external links

--- src/components/SidebarNavItem/SidebarNavItem.js
+++ src/components/SidebarNavItem/SidebarNavItem.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Link as RouterLink } from 'react-router-dom';
 
+import Link from '../Link/Link';
 import { connectTheme } from '../../utils';
 import Text from '../Text/Text';
 
@@ -18,19 +18,13 @@ class SidebarNavItem extends PureComponent {
       pathname: PropTypes.string,
       pathName: PropTypes.string,
       subItem: PropTypes.bool,
-      theme: PropTypes.object.isRequired,
-      match: PropTypes.shape({
-        isExact: PropTypes.bool,
-        path: PropTypes.string,
-        url: PropTypes.string,
-        params: PropTypes.object
-      })
+      theme: PropTypes.object.isRequired
     };
   }
 
   onGetActive(name, pathname) {
     const { theme } = this.props;
-    return pathname.slice(1) === name ? theme.sidebar.itemActive : '';
+    return pathname === name ? theme.sidebar.itemActive : '';
   }
 
   render() {
@@ -42,15 +36,14 @@ class SidebarNavItem extends PureComponent {
       children,
       pathname, // path from window.location object
       pathName, // name from metadata to use as path for link
-      theme,
-      match = { url: '/' } // comes from React Router
+      theme
     } = this.props;
     const defaultPath = pathName ? pathName : name;
     const activeCss = this.onGetActive(defaultPath, pathname);
     const path = pathName ? pathName : name;
     return (
-      <RouterLink
-        to={`${match.url}${path}`}
+      <Link
+        to={path}
         className={`${theme.sidebar.link} nav-item sidebar-link ${subItem
           ? theme.sidebar.subItem
           : ''}`}
@@ -62,7 +55,7 @@ class SidebarNavItem extends PureComponent {
           <Text>{displayName ? displayName : name}</Text>
           {children}
         </div>
-      </RouterLink>
+      </Link>
     );
   }
 }
