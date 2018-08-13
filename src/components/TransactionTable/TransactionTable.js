536b11dc11268d87098699693522670a9f178cea
mark tyneway <mark@purse.io>
2018-08-13 12:21:08 -0700

    progress: parse and render data from uxtx class

--- src/components/TransactionTable/TransactionTable.js
+++ src/components/TransactionTable/TransactionTable.js
@@ -11,50 +11,74 @@ import TransactionView from './TransactionView';
 class TransactionTable extends PureComponent {
   constructor() {
     super();
-    // TODO: figure out way to accept
-    // certain props and render those rows
-    this.headers = [
-      'Date',
-      'Send/Receive',
-      'Wallet',
-      'Amount',
-      'Account',
-      'Confirmations',
-      'Recipient',
-    ];
-    this.expandHeight = 250;
+    this.txnManager = TxnManager.fromOptions(TxnManagerOptions);
 
-    this.txnManager = TxnManager.fromOptions(TransactionManagerOptions);
+    // TODO: use for caching purposes
+    this._headers = null;
   }
 
   static get propTypes() {
     return {
       transactions: propTypes.array,
+      wallet: propTypes.string,
+      headerMap: propTypes.object,
+      expandHeight: propTypes.number, // pass along to Table
     };
   }
   static get defaultProps() {
     return {
       // list of transactions from bcoin api
       transactions: [],
+      wallet: null,
+      // map of bpanel-utils.UXTX.toJSON output -> table header names
+      headerMap: {
+        date: 'Date',
+        uxtype: 'Send/Receive',
+        wallet: 'Wallet',
+        amount: 'Amount',
+        account: 'Account',
+        confirmations: 'Confirmations',
+        recipient: 'Recipient',
+      },
+      expandHeight: 250,
     };
   }
 
   formatTableData(transactions, wallet) {
-    const txns = this.txnManager.parse(data, wallet);
+    const txns = this.txnManager.parse(transactions, wallet);
     // TODO: build expandedData from txns
-    return txns;
+    const { headerMap } = this.props;
+
+    const tableInput = txns.map(tx => {
+      // map tx data to desired headers
+      const r = {};
+      for (let [key, val] of Object.entries(headerMap)) r[val] = tx[key];
+      return r;
+    });
+    return tableInput;
+  }
+
+  // TODO: cache result and return
+  getHeaders() {
+    const headers = Object.entries(this.props.headerMap).map(
+      ([key, val]) => val
+    );
+    return headers;
   }
 
   render() {
-    const { transactions } = this.props;
+    const { transactions, wallet } = this.props;
 
-    const tableData = this.formatTableData(transactions);
+    const tableData = this.formatTableData(transactions, wallet);
+
+    // TODO: only invoke if headerMap is different
+    const headers = this.getHeaders();
 
     // TODO: handle onRowClick
     return (
       <div>
         <Table
-          colHeaders={this.headers}
+          colHeaders={headers}
           tableData={tableData}
           onRowClick={e => console.log(e)}
         />
@@ -66,7 +90,7 @@ class TransactionTable extends PureComponent {
 export default connectTheme(TransactionTable);
 
 /*
- *
+ * TODO: implement expanded row view
   expandedHeight={this.expandHeight}
   styles={styles.selectListStyle}
   expandedData={null}
